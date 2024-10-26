d395b517ba58abdbecb9a0638850cc3a55f2faf1
FrancescoGiommi <francescogiommi97@icloud.com>
2024-10-26 17:30:05 +0200

    Add new array

--- ex-js-functions-snack-main/01 - Iniziali/script.js
+++ ex-js-functions-snack-main/01 - Iniziali/script.js
@@ -1,16 +1,21 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-console.log(names);
 
-const extractFirstLetter = (array) => {
-  const firstLetter = [];
-  for (let i = 0; i < array.lenght; i++) {
-    firstLetter = [names[i]];
-  }
-  return firstLetter;
-};
-console.log(extractFirstLetter(names));
+/* Invoca la funzione qui e stampa il risultato in console */
 
-// Invoca la funzione qui e stampa il risultato in console
-//Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+function extraxtFirstLetter(words) {
+  const getFirstLetter = [];
+
+  words.forEach((word) => {
+    const firstLetter = word[0];
+    getFirstLetter.push(firstLetter);
+  });
+
+  return getFirstLetter;
+}
+
+const getFirstLetter = extraxtFirstLetter(names);
+
+console.log(getFirstLetter);
+/* Risultato atteso: ["A", "L", "M", "A", "G", "A"] */
