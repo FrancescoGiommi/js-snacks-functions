512a4e55d31b64d2a417329a7db948af15b93d38
FrancescoGiommi <francescogiommi97@icloud.com>
2024-10-27 18:38:38 +0100

    Add fix

--- ex-js-functions-snack-main/03 - conta vocali/script.js
+++ ex-js-functions-snack-main/03 - conta vocali/script.js
@@ -1,13 +1,19 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
+function getVocals(text) {
+  const vocalArray = ["a", "e", "i", "o", "u"];
+  const charArray = text.split("");
+  let vocalIndex = 0;
 
+  charArray.forEach((char) => {
+    if (vocalArray.includes(char)) vocalIndex++;
+  });
 
+  return vocalIndex;
+}
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+console.log(getVocals(word));
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
