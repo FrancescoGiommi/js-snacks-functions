ae48344ea5e24f7baa38d908b4d2bff4930bbeee
FrancescoGiommi <francescogiommi97@icloud.com>
2024-10-27 16:49:21 +0100

    Add fix

--- ex-js-functions-snack-main/02 - Filtra iniziali/script.js
+++ ex-js-functions-snack-main/02 - Filtra iniziali/script.js
@@ -1,17 +1,16 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-let i = 0;
-// Dichiara la funzione qui.
 
-const letterA = names.filter(
-  (currentName, index) => index == names[0],
-  index == names[3],
-  index == names[5]
-);
+// Dichiara la funzione qui.
+function getNameByInitial(words, initial) {
+  const result = [];
+  words.forEach((word) => {
+    if (word[0] === initial) result.push(word);
+  });
+  return result;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
-console.log(letterA);
-
+console.log(getNameByInitial(names, "A"));
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
